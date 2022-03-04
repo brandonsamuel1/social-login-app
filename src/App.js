c9d7e76427429aa8ba267b4369d1c304ca1d0e5f
brandon-earls <brandonsamuel72@gmail.com>
2022-03-04 15:45:22 -0800

    Added routing for app. can now navigate to homepage, login page and register page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+
+import HomePage from './pages/homepage/homepage.component';
+import LoginPage from './pages/login/login.component';
+import RegisterPage from './pages/register/register.component';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Social Login App!</h1>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+      </Routes>
     </div>
   );
 }
