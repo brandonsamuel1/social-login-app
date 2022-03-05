39dad079d40ca2a9c773036221ef89d5d9e0413b
brandon-earls <brandonsamuel72@gmail.com>
2022-03-04 17:51:09 -0800

    Styled login and register page with tailwind css; styled homepage to be more consistent with app

--- src/pages/login/login.component.jsx
+++ src/pages/login/login.component.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
     return (
-        <div>
-            Login
+        <div class="min-h-screen flex justify-center items-center">
+            <div class="py-12 px-12 bg-white rounded-2xl shadow-xl z-20">
+                <div>
+                    <h1 class="text-3xl font-bold text-center mb-4 cursor-pointer">Sign in to your Account</h1>
+                </div>
+                <div class="space-y-4">
+                    <input type="text" placeholder="Email Addres" class="block text-sm py-3 px-4 rounded-lg w-full border outline-none" />
+                    <input type="text" placeholder="Password" class="block text-sm py-3 px-4 rounded-lg w-full border outline-none" />
+                </div>
+                <div class="text-center mt-6">
+                    <button class="py-3 w-64 text-xl text-white bg-blue-400 rounded-2xl">LOGIN</button>
+                    <p class="mt-4 text-sm">Dont Have An Account? <Link to='/register'><span class="underline cursor-pointer"> Register</span></Link>
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/register/register.component.jsx
+++ src/pages/register/register.component.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
     return (
-        <div>
-            Register
+        <div class="min-h-screen flex justify-center items-center">
+            <div class="py-12 px-12 bg-white rounded-2xl shadow-xl z-20">
+                <div>
+                    <h1 class="text-3xl font-bold text-center mb-4 cursor-pointer">Register An Account</h1>
+                    <p class="w-80 text-center text-sm mb-8 font-semibold text-gray-700 tracking-wide cursor-pointer">Enter credentials to register</p>
+                </div>
+                <div class="space-y-4">
+                    <input type="text" placeholder="Full Name" class="block text-sm py-3 px-4 rounded-lg w-full border outline-none" />
+                    <input type="text" placeholder="Email Addres" class="block text-sm py-3 px-4 rounded-lg w-full border outline-none" />
+                    <input type="text" placeholder="Password" class="block text-sm py-3 px-4 rounded-lg w-full border outline-none" />
+                    <input type="text" placeholder="Confirm Password" class="block text-sm py-3 px-4 rounded-lg w-full border outline-none" />
+                </div>
+                <div class="text-center mt-6">
+                    <button class="py-3 w-64 text-xl text-white bg-blue-400 rounded-2xl">REGISTER</button>
+                    <p class="mt-4 text-sm">Already Have An Account? <Link to='/login'><span class="underline cursor-pointer"> Sign In</span></Link>
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
