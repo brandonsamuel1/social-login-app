2c9cfe78506750f36d17cc264365812eeaf839f1
brandon-earls <brandonsamuel72@gmail.com>
2022-03-04 17:24:48 -0800

    Added tailwind css for styles; styled homepage and linked login and register buttons to their respective pages

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,54 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import './homepage.styles.scss'
 
 const Home = () => {
     return (
-        <div>
-            <h1>Welcome</h1>
-            <p>Social Login App</p>
+        <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+            <div class="max-w-md w-full space-y-8 m-50">
+                <div>
+                    <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
+                    <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Welcome</h2>
+                    <br></br>
+                    <p>SOCIAL LOGIN APP</p>
+                </div>
+                <div class="mt-8 space-y-6">
+                    <Link to='/register'>
+                        <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                            <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+                                <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                    <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
+                                </svg>
+                            </span>
+                            REGISTER
+                        </button>
+                    </Link>
+                    <Link to='/login'>
+                        <button type="submit" class="group relative w-full flex justify-center mt-10 py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                            <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+                                <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                    <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
+                                </svg>
+                            </span>
+                            LOGIN
+                        </button>
+                    </Link>
+                </div>
+            </div>
         </div>
+        // <div>
+        //     <div className="container">
+        //         <div className="card-wrap">
+        //             <div className="card-body">
+        //                 <h1 className="card-title">Welcome</h1>
+        //                 <p className="card-message">Social Login App</p>
+        //                 <div className="register-btn">REGISTER</div>
+        //                 <div className="login-btn">LOGIN</div>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
     )
 }
 
