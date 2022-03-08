16014c065cd85f495b3e4fffaa0e9bad646b50d9
brandon-earls <brandonsamuel72@gmail.com>
2022-03-07 18:22:24 -0800

    Implemented google signin/register; cleaned up class name attributes

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import './homepage.styles.scss'
 
 const Home = () => {
     return (
-        <div class="min-h-screen flex justify-center items-center">
-            <div class="py-12 px-12 bg-white rounded-2xl shadow-xl z-20">
+        <div className="min-h-screen flex justify-center items-center">
+            <div className="py-12 px-12 bg-white rounded-2xl shadow-xl z-20">
                 <div>
-                    <h1 class="text-3xl font-bold text-center mb-4 cursor-pointer">Welcome</h1>
+                    <h1 className="text-3xl font-bold text-center mb-4 cursor-pointer">Welcome</h1>
                     <p>SOCIAL LOGIN APP</p>
                 </div>
-                <div class="text-center mt-6">
-                    <Link to='/register'><button class="py-3 w-64 text-xl text-white bg-blue-400 rounded-2xl">REGISTER</button></Link>
+                <div className="text-center mt-6">
+                    <Link to='/register'><button className="py-3 w-64 text-xl text-white bg-blue-400 rounded-2xl">REGISTER</button></Link>
                 </div>
-                <div class="text-center mt-6">
-                    <Link to='/login'><button class="py-3 w-64 text-xl text-white bg-blue-400 rounded-2xl">LOGIN</button></Link>
+                <div className="text-center mt-6">
+                    <Link to='/login'><button className="py-3 w-64 text-xl text-white bg-blue-400 rounded-2xl">LOGIN</button></Link>
                 </div>
             </div>
         </div>
--- src/pages/login/login.component.jsx
+++ src/pages/login/login.component.jsx
@@ -1,29 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { signInWithGoogle } from "../../firebase/firebase";
+
 const LoginPage = () => {
     return (
-        <div class="min-h-screen flex justify-center items-center">
-            <div class="py-12 px-12 bg-white rounded-2xl shadow-xl z-20">
+        <div className="min-h-screen flex justify-center items-center">
+            <div className="py-12 px-12 bg-white rounded-2xl shadow-xl z-20">
                 <div>
-                    <h1 class="text-3xl font-bold text-center mb-4">Sign in to your Account</h1>
-                    <p class="w-80 text-center text-sm mb-8 font-semibold text-gray-700 tracking-wide">Enter credentials to login</p>
-                    <div class="space-x-4">
-                        <button class="border-2 border-green-600 text-black px-4 py-2 rounded-md text-1xl font-medium hover:bg-green-600 transition duration-300">Google</button>
-                        <button class="border-2 border-blue-600 text-black px-4 py-2 rounded-md text-1xl font-medium hover:bg-blue-600 transition duration-300">Facebook</button>
-                        <button class="border-2 border-gray-600 text-black px-4 py-2 rounded-md text-1xl font-medium hover:bg-gray-600 transition duration-300">Github</button>
+                    <h1 className="text-3xl font-bold text-center mb-4">Sign in to your Account</h1>
+                    <p className="w-80 text-center text-sm mb-8 font-semibold text-gray-700 tracking-wide">Enter credentials to login</p>
+                    <div className="space-x-4">
+                        <button onClick={signInWithGoogle} className="border-2 border-green-600 text-black px-4 py-2 rounded-md text-1xl font-medium hover:bg-green-600 transition duration-300">Google</button>
+                        <button className="border-2 border-blue-600 text-black px-4 py-2 rounded-md text-1xl font-medium hover:bg-blue-600 transition duration-300">Facebook</button>
+                        <button className="border-2 border-gray-600 text-black px-4 py-2 rounded-md text-1xl font-medium hover:bg-gray-600 transition duration-300">Github</button>
                     </div>
                     <br />
                     <p>or</p>
                 </div>
                 <br />
-                <div class="space-y-4">
-                    <input type="text" placeholder="Email Addres" class="block text-sm py-3 px-4 rounded-lg w-full border outline-none" />
-                    <input type="text" placeholder="Password" class="block text-sm py-3 px-4 rounded-lg w-full border outline-none" />
+                <div className="space-y-4">
+                    <input type="text" placeholder="Email Addres" className="block text-sm py-3 px-4 rounded-lg w-full border outline-none" />
+                    <input type="text" placeholder="Password" className="block text-sm py-3 px-4 rounded-lg w-full border outline-none" />
                 </div>
-                <div class="text-center mt-6">
-                    <button class="py-3 w-64 text-xl text-white bg-blue-400 rounded-2xl">LOGIN</button>
-                    <p class="mt-4 text-sm">Dont Have An Account? <Link to='/register'><span class="underline cursor-pointer"> Register</span></Link>
+                <div className="text-center mt-6">
+                    <button className="py-3 w-64 text-xl text-white bg-blue-400 rounded-2xl">LOGIN</button>
+                    <p className="mt-4 text-sm">Dont Have An Account? <Link to='/register'><span className="underline cursor-pointer"> Register</span></Link>
                     </p>
                 </div>
             </div>
